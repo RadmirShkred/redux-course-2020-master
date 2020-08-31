c44834ebb11c846e4ecf7f86937700fba69900e6
radmir-shkred <psitecon@gmail.com>
2020-09-01 00:40:51 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import {applyMiddleware, createStore, compose} from 'redux';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import './styles.css';
+import {rootReducer} from "./redux/rootReducer";
+import {asyncIncrement, changeTheme, decrement, increment} from "./redux/actions";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+const counter = document.getElementById('counter');
+const addBtn = document.getElementById('add');
+const subBtn = document.getElementById('sub');
+const asyncBtn = document.getElementById('async');
+const themeBtn = document.getElementById('theme');
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(thunk, logger),
+    )
+);
+
+addBtn.addEventListener('click', () => {
+    store.dispatch(increment());
+});
+
+subBtn.addEventListener('click', () => {
+    store.dispatch(decrement());
+});
+
+asyncBtn.addEventListener('click', () => {
+    store.dispatch(asyncIncrement());
+});
+
+themeBtn.addEventListener('click', () => {
+    const newTheme = document.body.classList.contains('light') ? 'dark' : 'light';
+    store.dispatch(changeTheme(newTheme));
+    // document.body.classList.toggle('dark');
+});
+
+store.subscribe(() => {
+    const state = store.getState();
+    counter.textContent = state.counter;
+    document.body.className = state.theme.value;
+    [addBtn, subBtn, asyncBtn, themeBtn].forEach(btn => btn.disabled = state.theme.disabled);
+});
+
+store.dispatch({type: 'INIT_APPLICATION'});
